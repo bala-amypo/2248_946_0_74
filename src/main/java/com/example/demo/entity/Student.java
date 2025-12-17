4320af9c9b94984eb97f4db882e423c6abb846c3
bala-amypo <bala@amypo.in>
2025-12-17 06:09:22 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,34 +1,64 @@
-e com.example.demo.entity;
+package com.example.demo.entity;
 
-public class Student{
-    private long id;
-    private String name;
-    private String email;
-    private float cgpa;
+import jakarta.persistence.*;
 
-    public long getId(){
-        return id;
-    }
-    public void setId(long id){
-        this.id=id;
-    }
-    public String getName(){
-        return name;
-    }
-    public void setName(long name){
-        this.name=name;
-    }
-    public String getEmail(){
-        return email;
-    }
-    public void setEmail(long email){
-        this.email=email;
-    }
-    public float getCgpa(){
-        return cgpa;
-    }
-    public void setCgpa(long id){
-        this.cgpa=cgpa;
-    }
-   
+
+@Entity
+public class Student {
+  @Id
+  //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+  private String name;
+  private String email;
+  private float cgpa;
+  private String course;
+  public int getId() {
+      return id;
+  }
+
+  public void setId(int id) {
+      this.id = id;
+  }
+
+  public String getName() {
+      return name;
+  }
+
+  public void setName(String name) {
+      this.name = name;
+  }
+
+  public String getEmail() {
+      return email;
+  }
+
+  public void setEmail(String email) {
+      this.email = email;
+  }
+
+  public String getCourse() {
+      return course;
+  }
+
+  public void setCourse(String course) {
+      this.course = course;
+  }
+
+  public float getCgpa() {
+      return cgpa;
+  }
+
+  public void setCgpa(float cgpa) {
+      this.cgpa = cgpa;
+  }
+  public Student(int id, String name, String email, float cgpa, String course) {
+      this.id = id;
+      this.name = name;
+      this.email = email;
+      this.cgpa = cgpa;
+      this.course = course;
+  }
+  public Student(){
+    
+  }
 }
